075af964c0c12b47262a7188ac39f6378f38f890
shuo <caihehuang@gmail.com>
2023-04-28 23:47:40 +0800

    update - display logic

--- src/component/EmojiBg.jsx
+++ src/component/EmojiBg.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { emojisData } from './EmojiData'
 import './EmojiBg.scss'
 import useMousePosition from '../helper/hooks/useMousePosition'
+import { motion, AnimatePresence } from 'framer-motion'
+import uuid from 'react-uuid'
 
 function rdm(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min)
@@ -9,53 +11,79 @@ function rdm(min, max) {
 
 const EmojiItem = ({ pageX, pageY, text, fontSize, rotate }) => {
 	return (
-		<div
-			className="logo"
-			style={{
-				left: pageX,
-				top: pageY,
-				fontSize: fontSize,
-				// transform: `rotate(${rotate}deg) translate(-50%, 50%)`,
-				transform: `translate(-50%, -50%)`,
-			}}
-		>
-			{text}
-		</div>
+		<AnimatePresence mode="wait">
+			<motion.div
+				initial={{ scale: 0 }}
+				animate={{
+					scale: 1,
+				}}
+				exit={{
+					scale: 3,
+				}}
+				className="logo"
+				style={{
+					left: pageX,
+					top: pageY,
+					fontSize: fontSize,
+					rotate: rotate,
+					x: '-50%',
+					y: '-50%',
+				}}
+			>
+				{text}
+			</motion.div>
+		</AnimatePresence>
 	)
 }
 
 const EmojiBg = () => {
 	const mousePosition = useMousePosition()
-	const [emojiArray, setEmojiArray] = useState([
-		// {
-		// 	text: '',
-		// 	pageX: null,
-		// 	pageY: null,
-		// },
-	])
+	const [emojiArray, setEmojiArray] = useState([])
+	const [move, setMove] = useState(0)
 
-	console.log(emojiArray)
-
-	function becool() {
+	function becool(e) {
+		e.preventDefault()
+		setMove((pre) => pre + 1)
 		let text = emojisData[rdm(0, emojisData.length - 1)]
 		let pageX = mousePosition.x
 		let pageY = mousePosition.y
 		let fontSize = rdm(40, 160)
 		let rotate = rdm(0, 360)
-		setEmojiArray((pre) => [...pre, [text, pageX, pageY, fontSize, rotate]])
+		let id = uuid()
+
+		if (move % 10 === 0) {
+			setEmojiArray((pre) => [
+				...pre,
+				{
+					text: text,
+					pageX: pageX,
+					pageY: pageY,
+					fontSize: fontSize,
+					rotate: rotate,
+					id: id,
+				},
+			])
+		}
+
+		// if (emojiArray.length > 100) {
+		// 	// const newItems = [...emojiArray]
+		// 	// newItems.shift()
+		// 	setEmojiArray((pre) => pre.slice(1))
+		// 	// emojiArray.shift()
+		// }
 	}
 
 	return (
 		<div className="emoji-bg" onMouseMove={becool}>
-			{emojiArray.map((value, index) => {
+			{emojiArray.map((value) => {
 				return (
 					<EmojiItem
-						key={index}
-						text={value[0]}
-						pageX={value[1]}
-						pageY={value[2]}
-						fontSize={value[3]}
-						rotate={value[4]}
+						text={value.text}
+						pageX={value.pageX}
+						pageY={value.pageY}
+						fontSize={value.fontSize}
+						rotate={value.rotate}
+						key={value.id}
 					/>
 				)
 			})}
